322239e21b4dbed4b655891b2214ba9cba44fedc
Usuario6842 <136459003+Usuario6842@users.noreply.github.com>
2025-08-18 16:37:04 -0400

    style: adjust colors in SwitcherTheme component

--- src/components/SwitcherTheme.tsx
+++ src/components/SwitcherTheme.tsx
@@ -10,15 +10,15 @@ export const SwitcherTheme = () => {
       onClick={toggleTheme}
       className={cn(
         "relative w-16 h-8 flex items-center rounded-full cursor-pointer transition-colors duration-500",
-        theme === "light" ? "bg-yellow-900" : "bg-blue-900"
+        theme === "light" ? "bg-primary/30" : "bg-primary/80"
       )}
     >
       <div
         className={cn(
           "absolute size-6 rounded-full flex items-center justify-center transition-transform duration-500 shadow-md",
           theme === "light"
-            ? "translate-x-1 bg-yellow-500 text-gray-800"
-            : "translate-x-9 bg-blue-500 text-white"
+            ? "translate-x-1 text-primary bg-black/80"
+            : "translate-x-9 text-blue-500 bg-black/80"
         )}
       >
         {theme === "light" ? <SunIcon className="size-4" /> : <MoonIcon className="size-4" />}
